c7350d251b1b59c3dd0fd07ca152eb58a7510227
andre <andre.costa@tpv-tech.com>
2021-04-21 17:07:16 -0400

    fix remove category

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -9,18 +9,18 @@ export function create(req: Request, res: Response) {
 
 export function list(_: Request, res: Response) {
   return CategoryDAO.findAll()
-    .then((categories) => res.status(201).send(categories))
+    .then((categories) => res.status(200).send(categories))
     .catch((error) => res.status(500).json({ error: error }));
 }
 
 export function listOne(req: Request, res: Response) {
   return CategoryDAO.findById(Number(req.params.id))
-    .then((category) => res.status(201).send(category))
+    .then((category) => res.status(200).send(category))
     .catch((error) => res.status(500).json({ error: error }));
 }
 
 export function remove(req: Request, res: Response) {
   return CategoryDAO.remove(req.body.data)
-    .then((category) => res.sendStatus(201).send(category))
+    .then((_) => res.status(200).json({result: true}))
     .catch((error) => res.status(500).json({ error: error }));
 }
